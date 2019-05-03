5efb42b1d4f2fa77d25d4c6ea3d048a585c82061
Scott H <zenwattage@gmail.com>
2019-05-02 17:10:02 -0700

    table layout for train input and output display

--- assets/js/traintimer.js
+++ assets/js/traintimer.js
@@ -14,30 +14,12 @@ var config = {
 
   // -----------------------------
 
-// connectionsRef references a specific location in our database.
+// trainInfo references a specific location in our database.
 // All of our connections will be stored in this directory.
-var connectionsRef = database.ref("/connections");
-
-// '.info/connected' is a special location provided by Firebase that is updated
-// every time the client's connection state changes.
-// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-var connectedRef = database.ref(".info/connected");
-
-// When the client's connection state changes...
-connectedRef.on("value", function(snap) {
-
-  // If they are connected..
-  if (snap.val()) {
-
-    // Add user to the connections list.
-    var con = connectionsRef.push(true);
-    // Remove user from the connection list when they disconnect.
-    con.onDisconnect().remove();
-  }
-});
+var trainInfo = database.ref("/connections");
 
 // When first loaded or when the connections list changes...
-connectionsRef.on("value", function(snap) {
+trainInfo.on("value", function(snap) {
 
   // Display the viewer count in the html.
   // The number of online users is the number of children in the connections list.
